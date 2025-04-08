aa22a193f1bae142fb46df0f393050e86f925a14
Philip Bierley <P.bierley@yahoo.com>
2025-04-08 18:43:15 -0400

    gonna revert back once i break things lol

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const fs = require("fs");
-
+const cors = require("cors");
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 const port = 3000;
